e2bfedd1272fb2602c5e32d6b2c6676661e5d5f6
Mark Barry <markbarryty@hotmail.com>
2020-05-18 14:59:02 +0100

    Updates to UI

---
+++ SuperPowerApp/SuperPowerRegistry.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace SuperPowerApp
+{
+    partial class form_SuperHeroRegistry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_SuperHeroRegistry));
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.btn_Previous = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Exit.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Exit.Location = new System.Drawing.Point(984, 518);
+            this.btn_Exit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(198, 55);
+            this.btn_Exit.TabIndex = 0;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.on_Exit_Clicked);
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Next.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Next.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Next.Location = new System.Drawing.Point(704, 518);
+            this.btn_Next.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(198, 55);
+            this.btn_Next.TabIndex = 0;
+            this.btn_Next.Text = "Next";
+            this.btn_Next.UseVisualStyleBackColor = true;
+            this.btn_Next.Click += new System.EventHandler(this.on_Next_Clicked);
+            // 
+            // btn_Previous
+            // 
+            this.btn_Previous.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Previous.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Previous.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Previous.Location = new System.Drawing.Point(196, 518);
+            this.btn_Previous.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Previous.Name = "btn_Previous";
+            this.btn_Previous.Size = new System.Drawing.Size(198, 55);
+            this.btn_Previous.TabIndex = 0;
+            this.btn_Previous.Text = "Previous";
+            this.btn_Previous.UseVisualStyleBackColor = true;
+            this.btn_Previous.Click += new System.EventHandler(this.on_Previous_Clicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(491, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Super Power Registry";
+            // 
+            // form_SuperHeroRegistry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SuperPowerApp.Properties.Resources.folder_background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1200, 589);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Previous);
+            this.Controls.Add(this.btn_Next);
+            this.Controls.Add(this.btn_Exit);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "form_SuperHeroRegistry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Super Power Registry";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Button btn_Previous;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SuperPowerApp/SuperPowerRegistry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperPowerApp
+{
+    public partial class form_SuperHeroRegistry : Form
+    {
+        public form_SuperHeroRegistry()
+        {
+            InitializeComponent();
+        }
+
+        private void on_Exit_Clicked(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void on_Previous_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void on_Next_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
