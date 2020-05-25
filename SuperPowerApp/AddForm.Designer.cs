9a1bdae0c89afb20ed02e7a12cba66e9d7199163
Mark Barry <markbarryty@hotmail.com>
2020-05-25 13:05:28 +0100

    [Refactor] Forgot to push

--- SuperPowerApp/AddForm.Designer.cs
+++ SuperPowerApp/AddForm.Designer.cs
@@ -28,12 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_HeroName = new System.Windows.Forms.Label();
+            this.lbl_Affinity = new System.Windows.Forms.Label();
+            this.lbl_Region = new System.Windows.Forms.Label();
+            this.lbl_AliasInformation = new System.Windows.Forms.Label();
+            this.lbl_HeroInformation = new System.Windows.Forms.Label();
+            this.lbl_FirstName = new System.Windows.Forms.Label();
+            this.lbl_LastName = new System.Windows.Forms.Label();
+            this.lbl_Origin = new System.Windows.Forms.Label();
+            this.txtBox_HeroName = new System.Windows.Forms.TextBox();
+            this.txtBox_Affininty = new System.Windows.Forms.TextBox();
+            this.txtBox_Region = new System.Windows.Forms.TextBox();
+            this.txtBox_FirstName = new System.Windows.Forms.TextBox();
+            this.txtBox_LastName = new System.Windows.Forms.TextBox();
+            this.txtBox_Origin = new System.Windows.Forms.TextBox();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_HeroName
+            // 
+            this.lbl_HeroName.AutoSize = true;
+            this.lbl_HeroName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_HeroName.Location = new System.Drawing.Point(28, 68);
+            this.lbl_HeroName.Name = "lbl_HeroName";
+            this.lbl_HeroName.Size = new System.Drawing.Size(148, 18);
+            this.lbl_HeroName.TabIndex = 0;
+            this.lbl_HeroName.Text = "Super Hero Name:";
+            // 
+            // lbl_Affinity
+            // 
+            this.lbl_Affinity.AutoSize = true;
+            this.lbl_Affinity.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Affinity.Location = new System.Drawing.Point(28, 102);
+            this.lbl_Affinity.Name = "lbl_Affinity";
+            this.lbl_Affinity.Size = new System.Drawing.Size(154, 18);
+            this.lbl_Affinity.TabIndex = 0;
+            this.lbl_Affinity.Text = "Super Hero Affinity:";
+            // 
+            // lbl_Region
+            // 
+            this.lbl_Region.AutoSize = true;
+            this.lbl_Region.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Region.Location = new System.Drawing.Point(28, 137);
+            this.lbl_Region.Name = "lbl_Region";
+            this.lbl_Region.Size = new System.Drawing.Size(116, 18);
+            this.lbl_Region.TabIndex = 0;
+            this.lbl_Region.Text = "Active Region:";
+            // 
+            // lbl_AliasInformation
+            // 
+            this.lbl_AliasInformation.AutoSize = true;
+            this.lbl_AliasInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_AliasInformation.Location = new System.Drawing.Point(90, 224);
+            this.lbl_AliasInformation.Name = "lbl_AliasInformation";
+            this.lbl_AliasInformation.Size = new System.Drawing.Size(134, 18);
+            this.lbl_AliasInformation.TabIndex = 0;
+            this.lbl_AliasInformation.Text = "Alias Information";
+            // 
+            // lbl_HeroInformation
+            // 
+            this.lbl_HeroInformation.AutoSize = true;
+            this.lbl_HeroInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_HeroInformation.Location = new System.Drawing.Point(90, 29);
+            this.lbl_HeroInformation.Name = "lbl_HeroInformation";
+            this.lbl_HeroInformation.Size = new System.Drawing.Size(135, 18);
+            this.lbl_HeroInformation.TabIndex = 0;
+            this.lbl_HeroInformation.Text = "Hero Information";
+            // 
+            // lbl_FirstName
+            // 
+            this.lbl_FirstName.AutoSize = true;
+            this.lbl_FirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_FirstName.Location = new System.Drawing.Point(28, 255);
+            this.lbl_FirstName.Name = "lbl_FirstName";
+            this.lbl_FirstName.Size = new System.Drawing.Size(96, 18);
+            this.lbl_FirstName.TabIndex = 0;
+            this.lbl_FirstName.Text = "First Name:";
+            // 
+            // lbl_LastName
+            // 
+            this.lbl_LastName.AutoSize = true;
+            this.lbl_LastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_LastName.Location = new System.Drawing.Point(28, 286);
+            this.lbl_LastName.Name = "lbl_LastName";
+            this.lbl_LastName.Size = new System.Drawing.Size(94, 18);
+            this.lbl_LastName.TabIndex = 0;
+            this.lbl_LastName.Text = "Last Name:";
+            // 
+            // lbl_Origin
+            // 
+            this.lbl_Origin.AutoSize = true;
+            this.lbl_Origin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Origin.Location = new System.Drawing.Point(28, 320);
+            this.lbl_Origin.Name = "lbl_Origin";
+            this.lbl_Origin.Size = new System.Drawing.Size(53, 18);
+            this.lbl_Origin.TabIndex = 0;
+            this.lbl_Origin.Text = "Origin";
+            // 
+            // txtBox_HeroName
+            // 
+            this.txtBox_HeroName.Location = new System.Drawing.Point(181, 69);
+            this.txtBox_HeroName.Name = "txtBox_HeroName";
+            this.txtBox_HeroName.Size = new System.Drawing.Size(196, 20);
+            this.txtBox_HeroName.TabIndex = 1;
+            // 
+            // txtBox_Affininty
+            // 
+            this.txtBox_Affininty.Location = new System.Drawing.Point(181, 102);
+            this.txtBox_Affininty.Name = "txtBox_Affininty";
+            this.txtBox_Affininty.Size = new System.Drawing.Size(196, 20);
+            this.txtBox_Affininty.TabIndex = 1;
+            // 
+            // txtBox_Region
+            // 
+            this.txtBox_Region.Location = new System.Drawing.Point(181, 138);
+            this.txtBox_Region.Name = "txtBox_Region";
+            this.txtBox_Region.Size = new System.Drawing.Size(196, 20);
+            this.txtBox_Region.TabIndex = 1;
+            // 
+            // txtBox_FirstName
+            // 
+            this.txtBox_FirstName.Location = new System.Drawing.Point(181, 256);
+            this.txtBox_FirstName.Name = "txtBox_FirstName";
+            this.txtBox_FirstName.Size = new System.Drawing.Size(196, 20);
+            this.txtBox_FirstName.TabIndex = 1;
+            // 
+            // txtBox_LastName
+            // 
+            this.txtBox_LastName.Location = new System.Drawing.Point(181, 287);
+            this.txtBox_LastName.Name = "txtBox_LastName";
+            this.txtBox_LastName.Size = new System.Drawing.Size(196, 20);
+            this.txtBox_LastName.TabIndex = 1;
+            // 
+            // txtBox_Origin
+            // 
+            this.txtBox_Origin.Location = new System.Drawing.Point(181, 321);
+            this.txtBox_Origin.Name = "txtBox_Origin";
+            this.txtBox_Origin.Size = new System.Drawing.Size(196, 20);
+            this.txtBox_Origin.TabIndex = 1;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.BackColor = System.Drawing.Color.Snow;
+            this.btn_Add.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add.Location = new System.Drawing.Point(591, 378);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(165, 52);
+            this.btn_Add.TabIndex = 2;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = false;
+            // 
+            // AddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::SuperPowerApp.Properties.Resources.folder_background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(779, 445);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.txtBox_Origin);
+            this.Controls.Add(this.txtBox_LastName);
+            this.Controls.Add(this.txtBox_FirstName);
+            this.Controls.Add(this.txtBox_Region);
+            this.Controls.Add(this.txtBox_Affininty);
+            this.Controls.Add(this.txtBox_HeroName);
+            this.Controls.Add(this.lbl_Origin);
+            this.Controls.Add(this.lbl_LastName);
+            this.Controls.Add(this.lbl_FirstName);
+            this.Controls.Add(this.lbl_AliasInformation);
+            this.Controls.Add(this.lbl_Region);
+            this.Controls.Add(this.lbl_Affinity);
+            this.Controls.Add(this.lbl_HeroInformation);
+            this.Controls.Add(this.lbl_HeroName);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddForm";
             this.Text = "AddForm";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_HeroName;
+        private System.Windows.Forms.Label lbl_Affinity;
+        private System.Windows.Forms.Label lbl_Region;
+        private System.Windows.Forms.Label lbl_AliasInformation;
+        private System.Windows.Forms.Label lbl_HeroInformation;
+        private System.Windows.Forms.Label lbl_FirstName;
+        private System.Windows.Forms.Label lbl_LastName;
+        private System.Windows.Forms.Label lbl_Origin;
+        private System.Windows.Forms.TextBox txtBox_HeroName;
+        private System.Windows.Forms.TextBox txtBox_Affininty;
+        private System.Windows.Forms.TextBox txtBox_Region;
+        private System.Windows.Forms.TextBox txtBox_FirstName;
+        private System.Windows.Forms.TextBox txtBox_LastName;
+        private System.Windows.Forms.TextBox txtBox_Origin;
+        private System.Windows.Forms.Button btn_Add;
     }
 }
